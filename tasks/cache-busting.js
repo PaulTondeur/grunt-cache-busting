add817028e08a890c1a06d633e0e570057cb1f85
Paul Tondeur <paul@paultondeur.nl>
2013-10-30 16:38:49 +0100

    no double extensions in move file with hash

--- tasks/cache-busting.js
+++ tasks/cache-busting.js
@@ -1,6 +1,6 @@
-module.exports = function(grunt) {
-	var fs = require('fs'),
-		path = require('path'),
+module.exports = function (grunt) {
+	var fs     = require('fs'),
+		path   = require('path'),
 		crypto = require('crypto'),
 		gruntTextReplace = require('grunt-text-replace/lib/grunt-text-replace');
 
@@ -9,21 +9,19 @@ module.exports = function(grunt) {
 			hash = crypto.createHash('md5').update(fileContents).digest("hex"),
 			outputDir = path.dirname(this.data.file),
 			fileExtension = path.extname(this.data.file),
-			outputFile = outputDir + path.sep + this.data.replacement + "-" + hash + fileExtension,
 			replacementExtension = path.extname(this.data.replacement),
-			replacementWithoutExtension = this.data.replacement.slice(0, this.data.replacement.length - replacementExtension.length);
+			replacementWithoutExtension = this.data.replacement.slice(0, this.data.replacement.length - replacementExtension.length),
+			outputFile = outputDir + path.sep + replacementWithoutExtension + "-" + hash + fileExtension;
 
 		fs.rename(this.data.file, outputFile);
 
 		gruntTextReplace.replace({
 			src: this.data.replace,
 			overwrite: true,
-			replacements: [
-				{
-					from: this.data.replacement,
-					to: replacementWithoutExtension + "-" + hash + replacementExtension
-				}
-			]
+			replacements: [{
+				from: this.data.replacement,
+				to: replacementWithoutExtension + "-" + hash + replacementExtension
+			}]
 		});
 	});
-}
+};
